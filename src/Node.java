97344aaa0c00219db2ac39329088be5459d6769f
Tiago Prestes <bptiago123@gmail.com>
2025-03-25 18:29:47 -0300

    Upload local

---
+++ src/Node.java
@@ -0,0 +1,21 @@
+public class Node<E> {
+    private E element;
+    private Node<E> next;
+
+    public Node(E element) {
+        this.element = element;
+        this.next = null;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public E getElement() {
+        return element;
+    }
+}
